d93549b19e25c9097ee4761c388639c84cae2ae9
Josh Tucker <tucker1389@gmail.com>
2024-09-24 16:49:55 -0400

    Fixed sym names on Microsoft folders

--- UnnecessaryStartMenuShortcutsRemediate.ps1
+++ UnnecessaryStartMenuShortcutsRemediate.ps1
@@ -1,6 +1,6 @@
 #Removes unwanted start menu shortcuts
 #Written by Josh Tucker 9/5/2024
-$shortcutpaths= @("Windows Administrative Tools","Windows Ease of Access","Windows Accessories","Windows System","Microsoft Intune Management Extension","Ivanti")
+$shortcutpaths= @("Administrative Tools","Accessibility","Accessories","System Tools","Maintenance","Microsoft Intune Management Extension","Ivanti","Skype for Business.lnk")
 foreach($name in $shortcutpaths){
     $path = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\"+$name
     if(Test-Path $path){
