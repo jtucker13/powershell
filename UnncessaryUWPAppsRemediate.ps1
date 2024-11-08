a1d6aec1bb69250304d64a79a303c8d48647d5d9
Josh Tucker <tucker1389@gmail.com>
2024-11-08 14:55:09 -0500

    Added handling for provisioned packages

--- UnncessaryUWPAppsRemediate.ps1
+++ UnncessaryUWPAppsRemediate.ps1
@@ -1,8 +1,9 @@
 #Checks for the existence of unwanted UWP apps and exits if found
 #Written by Josh Tucker 8/8/2024
-$appnames= @("Microsoft.Windows.DevHome","Microsoft.BingWeather","Microsoft.ZuneMusic","Microsoft.GamingApp","Microsoft.XboxIdentityProvider","Microsoft.MicrosoftSolitaireCollection","Microsoft.Xbox.TCUI","Microsoft.XboxGameOverlay","Microsoft.WindowsCommunicationsApps","Microsoft.BingNews","Microsoft.ZuneVideo","Microsoft.FeedbackHub","Microsoft.GetHelp","Microsoft.XboxGamingOverlay","Microsoft.XBoxSpeechToTextOverlay","Microsoft.WindowsFeedbackHub","Microsoft.MixedReality.Portal","Microsoft.Getstarted","Microsoft.YourPhone","Microsoft.OneConnect")
+$appnames= @("MicrosoftTeams","Microsoft.549981C3F5F10","Microsoft.Copilot","Microsoft.Windows.DevHome","Microsoft.BingWeather","Microsoft.ZuneMusic","Microsoft.GamingApp","Microsoft.XboxIdentityProvider","Microsoft.MicrosoftSolitaireCollection","Microsoft.Xbox.TCUI","Microsoft.XboxGameOverlay","Microsoft.WindowsCommunicationsApps","Microsoft.BingNews","Microsoft.ZuneVideo","Microsoft.FeedbackHub","Microsoft.GetHelp","Microsoft.XboxGamingOverlay","Microsoft.XBoxSpeechToTextOverlay","Microsoft.WindowsFeedbackHub","Microsoft.MixedReality.Portal","Microsoft.Getstarted","Microsoft.YourPhone","Microsoft.OneConnect")
 foreach($name in $appnames){
     $app = Get-AppxPackage -Name $name -AllUsers
+    $appprov = Get-AppxProvisionedPackage -Online|Where-Object{$_.DisplayName -eq $name}
     if($app){
         Write-Host "$name detected installed"
         $app|Remove-AppxPackage -AllUsers
@@ -10,5 +11,11 @@ foreach($name in $appnames){
     else{
         Write-Host "$name not detected"
     }
+    if($appprov){
+        Write-Host "$name provisioning package installed"
+        $appprov|Remove-AppxProvisionedPackage -Online
+    }
+    else{
+        Write-Host "$name provisioning package not detected"
+    }
 }
-Get-AppxPackage -Na
--- UnnecessaryUWPAppsDetection.ps1
+++ UnnecessaryUWPAppsDetection.ps1
@@ -1,13 +1,19 @@
 #Checks for the existence of unwanted UWP apps and exits if found
 #Written by Josh Tucker 8/8/2024
-$appnames= @("Microsoft.Windows.DevHome","Microsoft.BingWeather","Microsoft.ZuneMusic","Microsoft.GamingApp","Microsoft.XboxIdentityProvider","Microsoft.MicrosoftSolitaireCollection","Microsoft.Xbox.TCUI","Microsoft.XboxGameOverlay","Microsoft.WindowsCommunicationsApps","Microsoft.BingNews","Microsoft.ZuneVideo","Microsoft.FeedbackHub","Microsoft.GetHelp","Microsoft.XboxGamingOverlay","Microsoft.XBoxSpeechToTextOverlay","Microsoft.WindowsFeedbackHub","Microsoft.MixedReality.Portal","Microsoft.Getstarted","Microsoft.YourPhone","Microsoft.OneConnect")
+$appnames= @("MicrosoftTeams","Microsoft.549981C3F5F10","Microsoft.Copilot","Microsoft.Windows.DevHome","Microsoft.BingWeather","Microsoft.ZuneMusic","Microsoft.GamingApp","Microsoft.XboxIdentityProvider","Microsoft.MicrosoftSolitaireCollection","Microsoft.Xbox.TCUI","Microsoft.XboxGameOverlay","Microsoft.WindowsCommunicationsApps","Microsoft.BingNews","Microsoft.ZuneVideo","Microsoft.FeedbackHub","Microsoft.GetHelp","Microsoft.XboxGamingOverlay","Microsoft.XBoxSpeechToTextOverlay","Microsoft.WindowsFeedbackHub","Microsoft.MixedReality.Portal","Microsoft.Getstarted","Microsoft.YourPhone","Microsoft.OneConnect")
 foreach($name in $appnames){
-    $app = Get-AppxPackage -Name $name -AllUsers
+    $app = Get-AppxPackage -Name $name 
+    $appprov = Get-AppxProvisionedPackage -Online|Where-Object{$_.DisplayName -eq $name}
     if($app){
         Write-Host "$name detected installed"
         Exit 1
     }
-    else{
+    elseif($appprov){
+        Write-Host "$name provisioned package detected installed"
+        Exit 1
+    }
+    else
+    {
         Write-Host "$name not detected"
     }
 }
