b22871dbc7da2dcba0f7daff7fb9cba6c78c1671
Josh Tucker <tucker1389@gmail.com>
2024-11-14 14:14:04 -0500

    Updated shell folder location

--- NFSShellFolderDetection.ps1
+++ NFSShellFolderDetection.ps1
@@ -1,6 +1,6 @@
 #Checks shell folders for the existence of an NFS path and exits if found
 #Written by Josh Tucker 6/25/2024
-$shellfolderpath="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+$shellfolderpath="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
 $usershellmappings= @("Personal","My Pictures","My Music","My Videos","Favorites")
 foreach($usm in $usershellmappings){
     $value = Get-ItemPropertyValue -Path $shellfolderpath -Name $usm -ErrorAction SilentlyContinue
