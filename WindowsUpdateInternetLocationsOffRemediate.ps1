7f4b152a9bb245f75a93218fd3db4a2abd9b1f2e
Josh Tucker <tucker1389@gmail.com>
2025-05-14 10:47:54 -0400

    updated to restart service after setting regkey to 0

--- WindowsUpdateInternetLocationsOffRemediate.ps1
+++ WindowsUpdateInternetLocationsOffRemediate.ps1
@@ -1,4 +1,5 @@
 #Removes old regkey for blocking access to WU internet locations, used for fixing issues with Company Portal installing
 #Written by Josh Tucker 5/13/25
 $shellfolderpath="HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
-Remove-ItemProperty -Path $shellfolderpath -Name "DoNotConnectToWindowsUpdateInternetLocation" -ErrorAction SilentlyContinue
+Set-ItemProperty -Path $shellfolderpath -Name "DoNotConnectToWindowsUpdateInternetLocation" -Value 0 -ErrorAction SilentlyContinue
+Restart-Service -Name "wuauserv"
