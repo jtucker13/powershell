f51a404c64e19ec9032722d5db86c253d5452ce5
Josh Tucker <tucker1389@gmail.com>
2025-05-26 09:29:43 -0400

    Fixed variable bug on nfs detection script

--- NFSShellFolderDetection.ps1
+++ NFSShellFolderDetection.ps1
@@ -25,14 +25,14 @@ function GetDriveSize{
     )
     $exclude ="$path*\Downloads\*"
     $size = Get-ChildItem -Path $path -Recurse | Where-Object {$_.fullname -notlike $exclude} | Measure-Object -Property Length -Sum
-    return $size/1MB 
+    return $($size.Sum)/1MB 
 }
 $shellfolderpath="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
 $usershellmappings= @("Personal","My Pictures","My Music","My Videos","Favorites","{374DE290-123F-4565-9164-39C4925E467B}","{F42EE2D3-909F-4907-8871-4C22FC0BF756}","{0DDD015D-B06C-45D5-8C4C-F59713854639}","{A0C69A99-21C8-4671-8703-7934162FCF1D}","{35286A68-3C57-41A1-BBB1-0EAE73D76C95}","{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}")
 foreach($usm in $usershellmappings){
     $value = Get-ItemPropertyValue -Path $shellfolderpath -Name $usm -ErrorAction SilentlyContinue
     if($value -like '*\\*'){
-        $hdrive=GetHDrive -path $oldpath -log $logfile
+        $hdrive=GetHDrive -path $value -log $logfile
         $hdrivesize=GetDriveSize -path $hdrive -log $logfile
         Write-Host "NFS drive $hdrive detected in $usm shell folder with : $hdrivesize MB"
         Exit 1
